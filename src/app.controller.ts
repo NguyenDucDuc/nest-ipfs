7d1f5368534673e76335612364f16c073453f65c
nguyenducduc <nguyenducduc26797@gmail.com>
2024-08-08 16:35:14 +0700

    init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  OnModuleInit,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service.js';
+import { HeliaLibp2p, createHelia } from 'helia';
+import { UnixFS, unixfs } from '@helia/unixfs';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Express } from 'express';
 
 @Controller()
-export class AppController {
+export class AppController implements OnModuleInit {
+  private helia: HeliaLibp2p;
+  private fs: UnixFS;
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  async getHeliaVersion(): Promise<string> {
-    const helia = await this.appService.getHelia();
-    return 'Helia is running, PeerId ' + helia.libp2p.peerId.toString();
+  async onModuleInit() {
+    this.helia = await createHelia();
+    this.fs = unixfs(this.helia);
   }
 
-  async onApplicationShutdown(): Promise<void> {
-    await this.appService.onApplicationShutdown();
+  @UseInterceptors(FileInterceptor('image'))
+  @Post()
+  async uploadImage(@UploadedFile() image: Express.Multer.File) {
+    const res = await this.fs.addBytes(image.buffer);
+    return { url: `${res}.ipfs.localhost:8080` };
   }
 }
